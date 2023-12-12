ab429cedeb33712dedda86b689d983224e147223
Rick South <rick@natamus.com>
2023-12-12 09:49:43 +0100

    Inventory Totem source code for 1.20.3.

---
+++ Forge/src/main/java/com/natamus/inventorytotem/ModForge.java
@@ -0,0 +1,32 @@
+package com.natamus.inventorytotem;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.inventorytotem.forge.events.ForgeTotemEvent;
+import com.natamus.inventorytotem.util.Reference;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+    	MinecraftForge.EVENT_BUS.register(new ForgeTotemEvent());
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
