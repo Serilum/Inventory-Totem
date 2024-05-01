8889f4861079b2603cafacd63abf6334c4cc2fcf
Rick South <rick@natamus.com>
2024-05-01 16:37:28 +0200

    Inventory Totem source code for 1.20.6.

---
+++ Common/src/main/java/com/natamus/inventorytotem/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.inventorytotem.util;
+
+public class Reference {
+	public static final String MOD_ID = "inventorytotem";
+	public static final String NAME = "Inventory Totem";
+	public static final String VERSION = "3.2";
+	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
+}
