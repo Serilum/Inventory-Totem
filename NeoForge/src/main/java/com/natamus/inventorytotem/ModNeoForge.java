b2f2ead347fa7e2df9ffa3719ba2e28cdf56ccd0
Rick South <rick@natamus.com>
2024-07-28 07:27:27 +0200

    (v3.3) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/inventorytotem/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/inventorytotem/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.inventorytotem;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.inventorytotem.neoforge.events.NeoForgeTotemEvent;
 import com.natamus.inventorytotem.util.Reference;
 import net.neoforged.neoforge.common.NeoForge;
@@ -12,6 +13,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
