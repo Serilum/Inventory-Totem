54840a76eca47e6def8ebe7d3ddcf2d75b1d7264
Rick South <rick@natamus.com>
2024-10-24 19:52:27 +0200

    Inventory Totem source code for 1.21.2.

---
+++ NeoForge/src/main/java/com/natamus/inventorytotem/ModNeoForge.java
@@ -0,0 +1,35 @@
+package com.natamus.inventorytotem;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.inventorytotem.neoforge.events.NeoForgeTotemEvent;
+import com.natamus.inventorytotem.util.Reference;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgeTotemEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
