0fc8418eca6cd9bf50af6839879a83a40e88d127
Rick South <rick@natamus.com>
2026-06-18 23:43:56 +0200

    Updated Inventory Totem to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/inventorytotem/util/Reference.java
+++ Common/src/main/java/com/natamus/inventorytotem/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "inventorytotem";
 	public static final String NAME = "Inventory Totem";
 	public static final String VERSION = "3.4";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
--- Forge/src/main/java/com/natamus/inventorytotem/ModForge.java
+++ Forge/src/main/java/com/natamus/inventorytotem/ModForge.java
@@ -4,7 +4,6 @@ import com.natamus.collective.check.RegisterMod;
 import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.inventorytotem.forge.events.ForgeTotemEvent;
 import com.natamus.inventorytotem.util.Reference;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.bus.BusGroup;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
